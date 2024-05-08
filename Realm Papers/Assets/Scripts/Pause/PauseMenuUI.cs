877d9b0620c9c4b1aace17469573d1c18947e13a
Fareza Athala <farezaathala10@gmail.com>
2024-05-08 08:50:30 +0700

    Fix Bug Pause Menu

--- Realm Papers/Assets/Scripts/Pause/PauseMenuUI.cs
+++ Realm Papers/Assets/Scripts/Pause/PauseMenuUI.cs
@@ -8,19 +8,9 @@ public class PauseMenuUI : MonoBehaviour
 {
     public GameObject pauseMenuPanel;
     public GameObject settingPanel;
-    public Button continueButton;
-    public Button settingButton;
-    public Button mainMenuButton;
 
     private bool isPause = false;
     private bool isSettingOpen = false;
-
-    void Start()
-    {
-        continueButton.onClick.AddListener(Continue);
-        settingButton.onClick.AddListener(OpenSetting);
-        mainMenuButton.onClick.AddListener(ToMainMenu);
-    }
 
     void Update()
     {
@@ -39,49 +29,35 @@ public class PauseMenuUI : MonoBehaviour
             {
                 Pause();
             }
-            
-            AudioManager.instance.PlaySFX("Klik Button");
         }
     }
 
     private void Continue()
     {
         pauseMenuPanel.SetActive(false);
-        Cursor.visible = false;
-
-        AudioManager.instance.PlaySFX("Klik Button");
         isPause = false;
     }
 
     private void Pause()
     {
         pauseMenuPanel.SetActive(true);
-        Cursor.visible = true;
-
-        AudioManager.instance.PlaySFX("Klik Button");
         isPause = true;
     }
 
     public void OpenSetting()
     {
         settingPanel.SetActive(true);
-
-        AudioManager.instance.PlaySFX("Klik Button");
         isSettingOpen = true;
     }
 
     public void CloseSetting()
     {
         settingPanel.SetActive(false);
-
-        AudioManager.instance.PlaySFX("Klik Button");
         isSettingOpen = false;
     }
 
     public void ToMainMenu()
     {
-        //GameManager.instance.TransitionScene("Main Menu");
         SceneManager.LoadScene("Main Menu");
-        AudioManager.instance.PlaySFX("Klik Button");
     }
 }
